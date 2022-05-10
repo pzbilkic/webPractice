3151122a70633b13b4684f34304a81f328f00fc0
pzbilkic <99282789+pzbilkic@users.noreply.github.com>
2022-05-09 23:16:31 -0700

    intial

---
+++ function.js
@@ -0,0 +1,9 @@
+function bounce(){
+    alert("x:");
+    var rectangle = Document.getElementById('rectangle');
+    rectangle.style.position = 'absolute';
+    rectangle.style.top = '100px';
+    rectangle.style.left = '200px';
+    document.getElementById('gfg').innerHTML = "worked";
+
+}
